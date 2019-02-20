0398a206516d3c00586b7e0393e47be97cf5d392
czq <chenzhengqiang@youzan.com>
2019-02-20 14:21:06 +0800

    feat: 添加typing

--- src/core/base/index.js
+++ src/core/base/index.js
@@ -5,7 +5,6 @@ const { base642Mat } = require('../../utils');
 const { TIP_COLOR, TIP_TEXT, LEVEL_INFO_MAP } = require('../../constants');
 const flagPool = require('../flag-pool');
 
-// 这里提供一些最基础的能力
 class BaseApp {
   constructor({ client = '', session = '', width = '', height = '' }) {
     this.client = client;
@@ -14,15 +13,6 @@ class BaseApp {
     this.height = height;
   }
 
-  /**
-   * 点击事件
-   * @param {number} x x坐标
-   * @param {number} y y坐标
-   * @param {boolean} needRandom 是否需要生成随机量
-   * @param {number} randX x坐标随机的量, 不填的话默认是5
-   * @param {number} randY y坐标随机的量, 不填的话跟randX一个值
-   * @returns {Promise<Function>}
-   */
   async tap(x, y, needRandom = false, randX, randY) {
     if (randX == null) {
       randX = 5;
@@ -45,10 +35,7 @@ class BaseApp {
       throw new Error ('啊哦，断掉了');
     }
   }
-  /**
-   * 执行动作链
-   * @param {Array<Object>} actions 动作链
-   */
+
   async chainOperation(actions) {
     try {
       await this.session.chainOperation(actions);
@@ -57,15 +44,6 @@ class BaseApp {
     }
   }
 
-  /**
-   * 拖拽
-   * @param {number} x1 前坐标x
-   * @param {number} y1 前坐标y
-   * @param {number} x2 后坐标x
-   * @param {number} y2 后坐标y
-   * @param {number} duration 耗时，秒为单位
-   * @return {null}
-   */
   async drag(x1, y1, x2, y2, duration = 0.7) {
     try {
       await this.session.swipe(x1, y1, x2, y2, duration);
@@ -74,12 +52,6 @@ class BaseApp {
     }
   }
 
-  /**
-   * 长按
-   * @param {number} x x坐标
-   * @param {number} y y坐标
-   * @return {null}
-   */
   async tapHold(x, y, delay = 1.0) {
     x = Math.round(x * 100) / 100;
     y = Math.round(y * 100) / 100;
@@ -91,12 +63,6 @@ class BaseApp {
     }
   }
 
-  /**
-   * 截屏
-   * @param {string} pathName 文件路径
-   * @param {boolean} needMat 是否需要返回Mat
-   * @return {cv::Mat | string}
-   */
   async screenshot(pathName = '', needMat = true) {
     // const msg = pathName !== '' ? ` pathName - ${pathName}` : '';
     try {
@@ -110,24 +76,8 @@ class BaseApp {
       throw new Error ('啊哦，断掉了');
     }
   }
-  /**
-   * 在父图片中查找子图片，获取相似度与起始坐标
-   *
-   * @typedef {Object} Point
-   * @property {number} x The X Coordinate
-   * @property {number} y The Y Coordinate
-   *
-   * @typedef {Object} MatchRes
-   * @property {number} simple simple value
-   * @property {Point} point point dot
-   *
-   * @param {CV::Mat} img1 子图片
-   * @param {CV::Mat} img2 父图片
-   * @param {boolean} needLog 是否需要打印日志，调试用
-   * @param {boolean} needShow 是否需要渲染结果，调试用
-   * @return {MatchRes}
-   */
-  judgeMatching(img1, img2, needLog = false, needShow = false) {
+
+  judgeMatching(img1, img2, needLog = false) {
     if (img1 == null || img2 == null) {
       throw new Error('图像不能为空！');
     }
@@ -139,17 +89,6 @@ class BaseApp {
     } = minMax;
 
     needLog && console.log(`maxSimple - ${maxVal.toFixed(2)}`);
-    // console.log(`points - [${x}, ${y}]`);
-    if (needShow) {
-      img1.drawRectangle(
-        new cv.Rect(x, y, img2.cols, img2.rows),
-        new cv.Vec(0, 255, 0),
-        2,
-        cv.LINE_8
-      );
-      cv.imshow('We\'ve found close!', img1);
-      cv.waitKey();
-    }
     // 之所以返回除以3，是因为屏幕缩放倍数的原因
     return {
       simple: maxVal,
@@ -157,11 +96,6 @@ class BaseApp {
     };
   }
 
-  /**
-   * 输出美化过的log
-   * @param {string} str 提示文案
-   * @param {Symbol} level 从constants里取
-   */
   log(str, level = LEVEL_INFO_MAP.info) {
     if (TIP_COLOR[level] != null) {
       const tip = TIP_COLOR[level](TIP_TEXT[level]);
@@ -178,11 +112,6 @@ class BaseApp {
     }
   }
 
-  /**
-   * 获取pic Mat
-   * @param {string} filePath 图片文件路径
-   * @return {cv::Mat}
-   */
   getPicture(filePath) {
     return flagPool.getFlag(filePath);
   }
