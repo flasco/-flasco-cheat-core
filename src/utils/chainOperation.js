0cb87597001d14d233218e06a97a9236806b5f3c
czq <chenzhengqiang@youzan.com>
2019-03-25 20:48:03 +0800

    feat: 添加拖拽的chain operation

--- src/utils/chainOperation.js
+++ src/utils/chainOperation.js
@@ -26,6 +26,40 @@ function wait(ms) {
   };
 }
 
+function drag([x1, y1], [x2, y2], druation, rand = 0) {
+  x1 = getRandom(x1, rand);
+  x2 = getRandom(x2, rand);
+  y1 = getRandom(y1, rand);
+  y2 = getRandom(y2, rand);
+
+  return [
+    {
+      action: 'press',
+      options: {
+        x: x1,
+        y: y1
+      }
+    },{
+      action: 'wait',
+      options: { ms: druation }
+    },
+    {
+      action: 'moveTo',
+      options: {
+        x: x2,
+        y: y2
+      }
+    },
+    {
+      action: 'wait',
+      options: { ms: 100 }
+    },
+    {
+      action: 'release'
+    }
+  ];
+}
+
 function tap(x, y, rand = 0) {
   x = getRandom(x, rand);
   y = getRandom(y, rand);
@@ -39,10 +73,11 @@ function tap(x, y, rand = 0) {
 }
 
 function getRandom(val, rand) {
-  const randomX = Math.random() * rand >>> 0;
+  const randomX = (Math.random() * rand) >>> 0;
   return val + randomX;
 }
 
 exports.tap = tap;
 exports.wait = wait;
+exports.drag = drag;
 exports.longPress = longPress;
--- typings/utils/chainOperation.d.ts
+++ typings/utils/chainOperation.d.ts
@@ -9,6 +9,14 @@ declare module '@flasco/cheat-core/src/utils/chainOperation' {
         };
       }
 
+      interface moveTo {
+        action: 'moveTo';
+        options: {
+          x: number;
+          y: number;
+        };
+      }
+
       interface tap {
         action: 'tap';
         options: {
@@ -39,7 +47,7 @@ declare module '@flasco/cheat-core/src/utils/chainOperation' {
       x: number,
       y: number,
       druation: number,
-      rand: number
+      rand?: number
     ): [actionInf.press, actionInf.wait, actionInf.release];
 
     /**
@@ -49,12 +57,25 @@ declare module '@flasco/cheat-core/src/utils/chainOperation' {
     export function wait(ms: number): actionInf.wait;
 
     /**
+     * 从起点拖拽至终点
+     * @param param0 起点
+     * @param param1 终点
+     * @param druation 拖拽的持续时间
+     */
+    export function drag(
+      [x1, y1]: Array<number>,
+      [x2, y2]: Array<number>,
+      druation: number,
+      rand?: number
+    ): actionInf.wait;
+
+    /**
      * 点击
      * @param x x坐标
      * @param y y坐标
      * @param rand 随机值
      */
-    export function tap(x: number, y: number, rand: number): actionInf.tap;
+    export function tap(x: number, y: number, rand?: number): actionInf.tap;
   }
 
   export = chainOperation;
